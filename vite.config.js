3d1e8a985935231bc5f7e89cb559d30430622777
Himani2615 <himanisingh26102005@gmail.com>
2024-07-13 21:38:11 +0530

    Vite config for env variables

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+    VITE_API_KEY:process.env.VITE_API_KEY,
+    VITE_ORGANIZATION_ID:process.env.VITE_ORGANIZATION_KEY,
+    VITE_APPID:process.env.VITE_APPID
+  },
   server: {
     proxy: {
       "/api": {
